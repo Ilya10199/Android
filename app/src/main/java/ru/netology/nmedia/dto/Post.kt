486a293c93d5b676731567ca4c63d6721303259f
Test <ilya99_99_99@mail.ru>
2024-08-21 12:13:57 +0300

    Corrected the rounding

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,7 +1,11 @@
 package ru.netology.nmedia
 
 import android.annotation.SuppressLint
-
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.abs
+import kotlin.math.ln
+import kotlin.math.pow
 
 
 data class Post(
@@ -9,31 +13,33 @@ data class Post(
         val author: String,
         val content: String,
         val published: String,
-        var likeCount: Int = 999,
+        var likeCount: Int = 1099,
         var shareCount: Int = 11_999,
         var visibilityCount: Int = 1_200_000,
         var likedByMe: Boolean = false
 
 )
-@SuppressLint("DefaultLocale")
-fun countFormat (count: Int): String {
-        val formatCount = when {
-                count in 1..999 -> {
-                        String.format(count.toString())
-                }
-                count in 1000..9999 -> {
-                        String.format("%.1fK", count / 1_000.0)
-                }
-                count in 10000..999999 -> {
-                        String.format("%dK", count / 1_000)
-                }
-                count >= 1000000 -> {
-                        String.format("%.1fM", count / 1_000_000.0)
-                }
 
-                else -> {
-                        count.toString()
-                }
+fun countFormat(likesCount: Int): String {
+        return when (likesCount) {
+                in 1000..1099 ->"${roundNoDecimal(likesCount.toDouble()/1_000.0)}K"
+                in 1100..9_999 ->"${roundWithDecimal(likesCount.toDouble()/1_000.0)}K"
+                in 10_000..999_999 ->"${roundNoDecimal(likesCount.toDouble()/1_000.0)}K"
+                in 1_000_000..1_099_999 ->"${roundNoDecimal(likesCount.toDouble()/1_000_000.0)}M"
+                in 1_100_000..Int.MAX_VALUE ->"${roundWithDecimal(likesCount.toDouble()/1_000_000.0)}M"
+
+                else-> likesCount.toString()
         }
-        return formatCount
+}
+
+
+fun roundWithDecimal(number: Double): String {
+        val df = DecimalFormat("#.#")
+        df.roundingMode = RoundingMode.DOWN
+        return df.format(number)
+}
+fun roundNoDecimal(number: Double): String {
+        val df = DecimalFormat("#")
+        df.roundingMode = RoundingMode.FLOOR
+        return df.format(number)
 }
